5cff93c20136eed35b6f226a60da83e8127ff724
ihorchub <103362130+ihorchub@users.noreply.github.com>
2022-10-31 10:52:03 +0000

    Deploying to gh-pages from @ ihorchub/cv-fullStackDeveloper-ihorChubov@0dee518e72a623c276629e4eb6ecf19b7786bfed 🚀

---
+++ index.af45f71e.js
@@ -0,0 +1,2 @@
+const t=document.querySelectorAll(".language"),e=document.querySelector(".lang-button");e.addEventListener("click",(function(){t.forEach((t=>{t.classList.toggle("is-hidden")})),"Українською"===e.textContent?e.textContent="In English":e.textContent="Українською"}));
+//# sourceMappingURL=index.af45f71e.js.map
---
+++ index.ccf5c806.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelectorAll(".language"),n=document.querySelector(".lang-button");n.addEventListener("click",(function(){t.forEach((function(t){t.classList.toggle("is-hidden")})),"Українською"===n.textContent?n.textContent="In English":n.textContent="Українською"}))}();
+//# sourceMappingURL=index.ccf5c806.js.map
