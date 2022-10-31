017ab5936307fa080eec2bf6620dec0db89b75ae
Ihor Chubov <ihor.chub@gmail.com>
2022-10-31 18:39:27 +0200

    add lang

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
-const lang = document.querySelectorAll('.language');
+import { langArr } from './js/lang';
 
-const langButton = document.querySelector('.lang-button');
+const select = document.querySelector('select');
+const allLang = ['en', 'uk'];
 
-langButton.addEventListener('click', onClick);
+location.href = `${location.pathname}#en`;
 
-function onClick() {
-  lang.forEach(elem => {
-    elem.classList.toggle('is-hidden');
-  });
-  if (langButton.textContent === 'Українською') {
-    langButton.textContent = 'In English';
-  } else {
-    langButton.textContent = 'Українською';
+select.addEventListener('change', changeURLLanguage);
+
+function changeURLLanguage() {
+  const lang = select.value;
+  location.href = `${location.pathname}#${lang}`;
+  changeLanguage();
+}
+
+function changeLanguage() {
+  let hash = window.location.hash.slice(1);
+  // if (!allLang.includes(hash)) {
+  //   location.href = `${location.pathname}#en`;
+  // }
+  select.value = hash;
+  // document.querySelector('title').innerHTML = langArr['unit'][hash];
+  // document.querySelector('.lng-chip').innerHTML = langArr['chip'][hash];
+  for (let key in langArr) {
+    let elem = document.querySelector(`.lng-${key}`);
+    if (elem) {
+      elem.innerHTML = langArr[key][hash];
+    }
   }
 }
---
+++ src/js/lang.js
@@ -0,0 +1,10 @@
+export const langArr = {
+  heading: {
+    en: 'the title of my resume',
+    uk: 'заголовок мого резюме',
+  },
+  text: {
+    en: 'resume of full stack developer Igor Chubov',
+    uk: 'резюме фул стек девелопера чубова ігоря',
+  },
+};
