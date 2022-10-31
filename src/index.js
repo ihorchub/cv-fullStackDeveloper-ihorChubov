e196946675897b818ea80c7c654c602a644b3d20
Ihor Chubov <ihor.chub@gmail.com>
2022-10-31 18:54:09 +0200

    change index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { langArr } from './js/lang';
 
 const select = document.querySelector('select');
-const allLang = ['en', 'uk'];
 
 location.href = `${location.pathname}#en`;
 
@@ -14,13 +13,10 @@ function changeURLLanguage() {
 }
 
 function changeLanguage() {
-  let hash = window.location.hash.slice(1);
-  // if (!allLang.includes(hash)) {
-  //   location.href = `${location.pathname}#en`;
-  // }
+  const hash = window.location.hash.slice(1);
+
   select.value = hash;
-  // document.querySelector('title').innerHTML = langArr['unit'][hash];
-  // document.querySelector('.lng-chip').innerHTML = langArr['chip'][hash];
+
   for (let key in langArr) {
     let elem = document.querySelector(`.lng-${key}`);
     if (elem) {
